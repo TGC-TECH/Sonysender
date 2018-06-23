a2013c5c8b089e0b03fcc4d81777741b8e2b4e5b
Toby Cowles <tobycowles1015@gmail.com>
2018-06-23 19:31:10 -0400

    Add files via upload

---
+++ SONYsender/SONYsender.ino
@@ -0,0 +1,41 @@
+/* send.ino Example sketch for IRLib2
+ *  Illustrates how to send a code.
+ */
+#include <IRLibSendBase.h>    // First include the send base
+//Now include only the protocols you wish to actually use.
+//The lowest numbered protocol should be first but remainder 
+//can be any order.
+#include <IRLib_P01_NEC.h>    
+#include <IRLib_P02_Sony.h>   
+#include <IRLibCombo.h>     // After all protocols, include this
+// All of the above automatically creates a universal sending
+// class called "IRsend" containing only the protocols you want.
+// Now declare an instance of that sender.
+
+IRsend mySender;
+int photocellPin = 0;     // the cell and 10K pulldown are connected to a0
+int photocellReading;     // the analog reading from the sensor divider
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println(F("Every time you press a key is a serial monitor we will send."));
+}
+
+void loop() {
+ 
+    //send a code every time a character is received from the 
+    // serial port. You could modify this sketch to send when you
+    // push a button connected to an digital input pin.
+    //Substitute values and protocols in the following statement
+    // for device you have available.
+     photocellReading = analogRead(photocellPin); 
+    Serial.print("Analog reading = ");
+  Serial.println(photocellReading);
+  delay(400);
+    if(photocellReading > 900) {
+    mySender.send(SONY,0x540C, 15);//Sony DVD power A8BCA, 20 bits
+    //mySender.send(NEC,0x61a0f00f,0);//NEC TV power button=0x61a0f00f
+    Serial.println(F("Sent signal."));
+   delay(5000); }
+}
+
